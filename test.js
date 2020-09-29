a5053a136126aec79821a064951ec46e4c44e3e4
asterka <askar.master@yandex.ru>
2020-09-29 13:22:18 -0400

    modified:   test.js

--- test.js
+++ test.js
@@ -4,7 +4,12 @@ const e = require('express');
 const app = express()
 
 //Initialize routes
-app.use(express.json())
+app.use(express.json());
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();}
+	);	
 app.use("/api",require("./routes/rooms"));
 
 let port = 3001;
